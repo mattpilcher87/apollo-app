4051e7c25d96ceaa2553443e44260c1d17918540
Matt Pilcher <matthew.pilcher@holidayextras.com>
2019-09-06 11:52:25 +0100

    Set up react / concurrently w server

---
+++ client/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Base from './containers/Base.js'
+
+ReactDOM.render(
+  <Base />
+  ,document.getElementById('root')
+)
---
+++ client/containers/Base.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Base extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default Base
